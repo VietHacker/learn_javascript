0f5695af533c3509ca16a727aa3b5af08533cb6e
tungls69 <97745758+tungls69@users.noreply.github.com>
2022-06-06 16:21:22 +0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+a=[
+    {
+        "name":"Viet",
+        "year":"2006"
+    },
+    {
+        "name":"adb",
+        "year":"2007"
+    },
+     {
+        "name":"tung",
+        "year":"1998"
+    }
+  ]
+
+let a2= a.map(x=>x.name+'a')
+console.log(a2)
+
+
